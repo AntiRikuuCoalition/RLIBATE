eff360eca75bcc881e966e8c6f046a97502ec4f0
ARCofficial <141395899+AntiRikuuCoalition@users.noreply.github.com>
2023-11-20 15:49:34 +0900

    Update AAAAA.js

--- AAAAA.js
+++ AAAAA.js
@@ -1,12 +1,12 @@
 function GRS() { 
-  const characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const characters = "0123456789​";
   let result = "";
   for (let i = 0; i < 3; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
 }
-var ip = GRS() + "." + GRS() + "." + GRS() + "." + GRS() + ".";
+var ip = GRS() + "." + GRS() + "." + GRS() + "." + GRS();
 console.log(ip);
 var data = {};
         data.uname = 'AAAAAAAAAAAA';
