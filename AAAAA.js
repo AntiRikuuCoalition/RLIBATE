16dc890cf628f56e85c450b738d5e24d18b45317
ARCofficial <141395899+AntiRikuuCoalition@users.noreply.github.com>
2023-11-20 15:29:59 +0900

    Update AAAAA.js

--- AAAAA.js
+++ AAAAA.js
@@ -1,6 +1,16 @@
-        var data = {};
+function GRS() { 
+  const characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let result = "";
+  for (let i = 0; i < 3; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+var ip = GRS() + "." + GRS() + "." + GRS() + "." + GRS() + ".";
+console.log(ip);
+var data = {};
         data.uname = 'AAAAAAAAAAAA';
         data.passwd = '1q2w3e';
-        data.bid = "IP抜かせません";
+        data.bid = ip;
         data.sid = fsid.get();
         socket.json.emit('login', data);
