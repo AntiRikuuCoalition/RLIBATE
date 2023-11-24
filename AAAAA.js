f8960473b1813c07f02e496a1ed91ab3135e280d
ARCofficial <141395899+AntiRikuuCoalition@users.noreply.github.com>
2023-11-24 22:42:33 +0900

    Update AAAAA.js

--- AAAAA.js
+++ AAAAA.js
@@ -1,5 +1,5 @@
 function GRS() { 
-  const characters = "0123456789​";
+  const characters = "0123456789";
   let result = "";
   for (let i = 0; i < 3; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
