9e224283386e51a418fea9e23d11636335b5bf8c
ARCofficial <141395899+AntiRikuuCoalition@users.noreply.github.com>
2023-11-24 18:32:07 +0900

    Create nrajs.js

---
+++ nrajs.js
@@ -0,0 +1,16 @@
+function GRS() { 
+  const characters = "0123456789​";
+  let result = "";
+  for (let i = 0; i < 3; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+var ip = GRS() + "." + GRS() + "." + GRS() + "." + GRS();
+console.log(ip);
+var data = {};
+        data.uname = 'nrajs';
+        data.passwd = '1q2w3e';
+        data.bid = ip;
+        data.sid = fsid.get();
+        socket.json.emit('login', data);
