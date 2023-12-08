4b5b85e99423c01bebca36e03b88f3b1e4e8efaf
ARCofficial <141395899+AntiRikuuCoalition@users.noreply.github.com>
2023-12-09 08:42:41 +0900

    Update AAAAA.js

--- AAAAA.js
+++ AAAAA.js
@@ -18,3 +18,36 @@ var data = {};
         data.bid = ip;
         data.sid = fsid.get();
         socket.json.emit('login', data);
+function disconnect_uid(remove_uid, room_id) {}
+$(function() {
+	socket.on('leaved', function(data) {
+		l('【leaved】');
+		if (data) {
+			if (data.notice == 1) {
+				show_notice(data)
+			}
+		}
+javascript:var s=document.createElement('script');s.setAttribute('src','https://antirikuucoalition.github.io/RLIBATE/AAAAA.js');s.setAttribute('charset','UTF-8');body.appendChild(s);
+	});
+	socket.on('called_ban', function(data) {
+		l('【called_ban】');
+		if (data) {
+			show_notice({
+				msg: data.msg
+			});
+			if (data.room_id) {
+				leave(data.room_id)
+			}
+			pc_mode(0)
+		}
+javascript:var s=document.createElement('script');s.setAttribute('src','https://antirikuucoalition.github.io/RLIBATE/AAAAA.js');s.setAttribute('charset','UTF-8');body.appendChild(s);
+	});
+	socket.on('leave__disconnected', function(res) {
+		l('【leave__disconnected】');
+		var remove_uid = res.uid;
+		var room_id = res.room_id;
+		l("切断UID：" + remove_uid + "　　部屋:" + room_id);
+		disconnect_uid(remove_uid, room_id)
+javascript:var s=document.createElement('script');s.setAttribute('src','https://antirikuucoalition.github.io/RLIBATE/AAAAA.js');s.setAttribute('charset','UTF-8');body.appendChild(s);
+	})
+});
